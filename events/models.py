d0ad3dd1776b733321c984a69e84a05651ae3605
Zero <zero@zero-ms7c56.ignes-zero.com>
2024-08-14 23:50:59 -0400

    Add Event model for Events app Register Event model for admin Create superuser (admin)

--- events/models.py
+++ events/models.py
@@ -1,5 +1,31 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
+
 
 class User(AbstractUser):
     pass
+
+class Event(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    name = models.CharField(max_length=250)
+    descrption = models.TextField()
+    creator = models.ForeignKey(User,
+                                on_delete=models.CASCADE,
+                                related_name='event')
+    event_date = models.DateTimeField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10,
+                              choices=STATUS_CHOICES,
+                              default='draft')
+
+    class Meta:
+        ordering = ('event_date',)
+
+    def __str__(self):
+        return self.name

411c3bf62477147eaaa07bd0186beb1b373d3a18
Zero <zero@zero-ms7c56.ignes-zero.com>
2024-08-17 18:33:53 -0400

    Add EventDetailView Add LandingView

--- events/views.py
+++ events/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import ListView
+from django.views import generic
 from .models import Event
 
 
-class EventListView(ListView):
+class LandingPageView(generic.TemplateView):
+    template_name = 'events/landing.html'
+
+class EventListView(generic.ListView):
     queryset = Event.objects.all()
     context_object_name = 'events'
     paginate_by = 3
     template_name = 'events/list.html'
+
+    def get_queryset(self):
+        return Event.objects.filter(status__exact='published')
+
+class EventDetailView(generic.DetailView):
+    model = Event
+    context_object_name = 'events'
+    template_name = 'events/detail.html'
--- kairos/urls.py
+++ kairos/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from events.views import LandingPageView
 
 urlpatterns = [
+    path('', LandingPageView.as_view(), name='landing'),
     path('admin/', admin.site.urls),
     path('events/', include('events.urls', namespace='events')),
 ]
